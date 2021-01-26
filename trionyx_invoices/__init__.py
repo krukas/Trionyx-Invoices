6163bba844427385bbff393df78c2368ad74fd60
Krukas <maikel@martens.me>
2021-01-26 13:49:45 +0100

    [TASK] Update version

--- trionyx_invoices/__init__.py
+++ trionyx_invoices/__init__.py
@@ -5,6 +5,6 @@ trionyx_invoices
 :copyright: 2019 by Maikel Martens
 :license: GPLv3
 """
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 default_app_config = 'trionyx_invoices.apps.InvoicesConfig'
